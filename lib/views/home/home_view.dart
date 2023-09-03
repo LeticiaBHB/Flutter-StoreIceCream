766ffb691966f44700b5fe6d4f6b29d024e5d05c
bowie <leticia.bini@gmail.com>
2023-09-03 05:05:35 -0300

    commit

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutterweb/widgets/call_to_action/call_to_action.dart';
 import 'package:flutterweb/widgets/centered_view/centered_view.dart';
+import 'package:flutterweb/widgets/course_details/course_details.dart';
 import 'package:flutterweb/widgets/navigation_bar/navigation_bar.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
@@ -12,6 +15,17 @@ class HomeView extends StatelessWidget {
         filho: Column(
           children: <Widget>[
             NavigatBar(),
+            Expanded(
+              child: Row(
+                children: [
+                  CourseDetails(),
+                  Expanded(
+                      child: Center(
+                        child: CallToAction('Meus projetos'),
+                      ) )
+                ],
+              ),
+            )
           ],
         ),
       ),
--- lib/widgets/course_details/course_details.dart
+++ lib/widgets/course_details/course_details.dart
@@ -5,13 +5,29 @@ class CourseDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 600,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [],
-      ),
+    return ListView(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width < 700 ? 400 : 700,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Desenvolvedora \n  Flutter❤️',
+                style: TextStyle(
+                    fontWeight: FontWeight.w800, height: 0.9, fontSize: 60),
+              ),
+              SizedBox(height: 30),
+              Expanded(
+                child: Text(
+                    'Como desenvolvedora mobile com expertise em Flutter, estou preparada para enfrentar desafios e criar soluções inovadoras para qualquer projeto de desenvolvimento mobile. Estou animada para continuar contribuindo para a comunidade de aplicativos móveis e transformando ideias em realidade por meio do poder desta tecnologia versátil e eficiente. Se você está em busca de uma profissional dedicada e apaixonada pelo desenvolvimento mobile, estou à disposição para colaborar em seu próximo projeto.',
+                    style: TextStyle(fontSize: 21, height: 1.7)),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
