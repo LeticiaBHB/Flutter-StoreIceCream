c44f99bc3261725d35c2ccac42abc583332456aa
bowie <leticia.bini@gmail.com>
2023-09-03 09:23:41 -0300

    commit

--- lib/widgets/course_details/course_details.dart
+++ lib/widgets/course_details/course_details.dart
@@ -24,14 +24,14 @@ class CourseDetails extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                'Desenvolvedora \n  Flutter❤️',
+                'Desenvolvedora \n ❤️',
                 style: TextStyle(
                     fontWeight: FontWeight.w800, height: 0.9, fontSize: titleSize),
                 textAlign: textAligment,
               ),
               SizedBox(height: 30),
               Text(
-                  'Como desenvolvedora mobile com expertise em Flutter, estou preparada para enfrentar desafios e criar soluções inovadoras para qualquer projeto de desenvolvimento mobile. Estou animada para continuar contribuindo para a comunidade de aplicativos móveis e transformando ideias em realidade por meio do poder desta tecnologia versátil e eficiente. Se você está em busca de uma profissional dedicada e apaixonada pelo desenvolvimento mobile, estou à disposição para colaborar em seu próximo projeto.',
+                  'Estou preparada para enfrentar desafios e criar soluções inovadoras para qualquer projeto de desenvolvimento. Estou animada para continuar contribuindo para a comunidade e transformando ideias em realidade por meio do poder desta tecnologia versátil e eficiente. Se você está em busca de uma profissional dedicada e apaixonada pelo desenvolvimento, estou à disposição para colaborar em seu próximo projeto.',
                   style: TextStyle(
                       fontSize: descriptionSize,
                       height: 1.7),
